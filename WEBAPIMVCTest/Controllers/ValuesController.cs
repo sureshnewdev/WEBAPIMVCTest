107063843e4f4deb819fa76b5347f8e0533adcb3
Murali <Murali@DESKTOP-B2PH9LN>
2022-04-08 21:48:51 +0530

    changed

--- WEBAPIMVCTest/Controllers/ValuesController.cs
+++ WEBAPIMVCTest/Controllers/ValuesController.cs
@@ -12,7 +12,7 @@ namespace WEBAPIMVCTest.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2", "Value3" };
+            return new string[] { "value1", "value2", "Value3","Value4","Value5","Value6" };
         }
 
         // GET api/values/5
